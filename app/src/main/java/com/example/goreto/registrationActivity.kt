7c77458448e587985fdf386ba9f01d216fae6dd8
beta_astra <kiran.211720@gmail.com>
2024-01-11 10:13:03 +0545

    first

---
+++ app/src/main/java/com/example/goreto/registrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.goreto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
