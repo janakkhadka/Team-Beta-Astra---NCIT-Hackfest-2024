2a68584e3ae5117698a5caf468516a8efda84189
Janak Khadka <108541554+janakkhadka@users.noreply.github.com>
2024-01-12 09:55:29 +0545

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,3 @@
-@rem
 @rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
@@ -87,3 +86,4 @@ exit /b 1
 if "%OS%"=="Windows_NT" endlocal
 
 :omega
+
